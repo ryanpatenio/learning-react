5592b1070a6346a720040770d0bdedb1440ddb8b
ryanpatenio <ryanpatenio06@gmail.com>
2025-04-23 19:38:14 +0800

    removed another layout -carLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import RootLayout from "./shared/Layouts/RootLayout"
-import CarLayout from "./shared/Layouts/CarLayout"
 import { Routes, Route } from "react-router-dom"
 
 import HomePage from './pages/Home'
@@ -14,15 +13,13 @@ const App = () => {
   return (  
      <Routes>
         <Route element={<RootLayout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/About" element={<AboutPage />} />
-            <Route path="/Cars" element={<CarsPage />} />
-              <Route element={<CarLayout />}>
-                  <Route path="/Cars/:id" element={<CarPage />} />
-              </Route>           
-            <Route path="/Contact" element={<ContactPage />} /> 
+          <Route path="/" element={<HomePage />} />
+          <Route path="/About" element={<AboutPage />} />
+          <Route path="/Cars" element={<CarsPage />} />             
+          <Route path="/Cars/:id" element={<CarPage />} />                        
+          <Route path="/Contact" element={<ContactPage />} /> 
         </Route> 
-      <Route path="*" element={<NotFoundPage />} />
+        <Route path="*" element={<NotFoundPage />} />
     </Routes>       
   )
 }
